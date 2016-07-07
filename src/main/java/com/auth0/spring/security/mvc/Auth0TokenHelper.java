003bb098d1e0d50d0b8b090b13ff5b0988ac4693
Richard Seldon <arcseldon@gmail.com>
2016-07-08 05:07:01 +0900

    Remove current codebase

--- src/main/java/com/auth0/spring/security/mvc/Auth0TokenHelper.java
+++
@@ -1,9 +0,0 @@
-package com.auth0.spring.security.mvc;
-
-public interface Auth0TokenHelper<T> {
-
-    public String generateToken(T object, long expiration);
-
-    public T decodeToken(String token);
-
-}
