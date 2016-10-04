d7c408386e22a65aa6523e8753234ea99b7cd8d4
Hernan Zalazar <hernan.zalazar@gmail.com>
2016-10-04 10:47:45 -0300

    Ignore IntelliJ files

--- src/main/java/com/auth0/spring/security/mvc/Auth0CallbackHandler.java
+++ src/main/java/com/auth0/spring/security/mvc/Auth0CallbackHandler.java
@@ -16,29 +16,15 @@ import java.io.IOException;
  *
  * Example usage - Simply extend this class and define Controller in subclass
  *
- *
- *  package com.auth0.example;
- *
- * import com.auth0.web.Auth0CallbackHandler;
- * import org.springframework.stereotype.Controller;
- * import org.springframework.web.bind.annotation.RequestMapping;
- * import org.springframework.web.bind.annotation.RequestMethod;
- *
- * import javax.servlet.ServletException;
- * import javax.servlet.http.HttpServletRequest;
- * import javax.servlet.http.HttpServletResponse;
- * import java.io.IOException;
- *
- *  @Controller
- *  public class CallbackController extends Auth0CallbackHandler {
- *
- *      @RequestMapping(value = "${auth0.loginCallback}", method = RequestMethod.GET)
- *      protected void callback(final HttpServletRequest req, final HttpServletResponse res)
- *                                                      throws ServletException, IOException {
- *          super.handle(req, res);
- *      }
- *  }
- *
+ * <pre><code>
+ * {@literal @}Controller
+ * public class CallbackController extends Auth0CallbackHandler {
+ *     {@literal @}RequestMapping(value = "${auth0.loginCallback}", method = RequestMethod.Get)
+ *     protected void callback(final HttpServletRequest req, final HttpServletResponse res) throws ServletException, IOException {
+ *         super.handle(req, res);
+ *     }
+ * }
+ * </code></pre>
  */
 @Component
 public class Auth0CallbackHandler {
